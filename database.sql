262034c6a22590060338e475ac9df4ea66fbd8c1
Nathan Field <d00235179@student.dkit.ie>
2023-11-27 18:07:25 +0000

    :ambulance: fix database queries (didnt accept backticks)

--- database.sql
+++ database.sql
@@ -1,13 +1,4 @@
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
---
--- Database: `plant_database`
---
-
 DROP TABLE IF EXISTS customer_invoice, plant, customer;
 DROP DATABASE IF EXISTS plant_database;
 CREATE DATABASE plant_database;
@@ -16,50 +7,45 @@ USE plant_database;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer`
+-- Table structure for table "customer"
 --
 
-CREATE TABLE `customer` (
-  `customer_id` int(11) NOT NULL PRIMARY KEY,
-  `customer_first_name` varchar(255) NOT NULL,
-  `customer_last_name` varchar(255) NOT NULL,
-  `customer_email_address` varchar(255) NOT NULL,
-  `customer_delivery_address` varchar(255) NOT NULL,
-  `phone_num` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
+CREATE TABLE customer (
+  customer_id int(11) NOT NULL PRIMARY KEY,
+  customer_first_name varchar(255) NOT NULL,
+  customer_last_name varchar(255) NOT NULL,
+  customer_email_address varchar(255) NOT NULL,
+  customer_delivery_address varchar(255) NOT NULL,
+  phone_num varchar(50) NOT NULL
+);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `plant`
+-- Table structure for table "plant"
 --
 
-CREATE TABLE `plant` (
-  `plant_id` int(11) NOT NULL PRIMARY KEY,
-  `plant_name` varchar(255) NOT NULL,
-  `plant_type` varchar(255) NOT NULL,
-  `plant_price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
+CREATE TABLE plant (
+	plant_id int(11) NOT NULL PRIMARY KEY,
+  plant_name varchar(255) NOT NULL,
+  plant_type varchar(255) NOT NULL,
+  plant_price float
+  );
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer_invoice`
+-- Table structure for table "customer_invoice"
 --
 
-CREATE TABLE `customer_invoice` (
-  `customer_id` int(11) NOT NULL,
-  `plant_id` int(11) NOT NULL,
-  `date_time_purchased` datetime NOT NULL,
-  `quantity` int(11) NOT NULL,
+CREATE TABLE customer_invoice (
+  customer_id int(11) NOT NULL,
+  plant_id int(11) NOT NULL,
+  date_time_purchased datetime NOT NULL,
+  quantity int(11) NOT NULL,
   FOREIGN KEY (plant_id) REFERENCES plant(plant_id),
   FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
+) ;
 
 -- Sample Insert Queries
 
@@ -73,4 +59,4 @@ Insert INTO plant VALUES (0, "Aloe Vera", "Succulent", 4.99),
 
 Insert INTO customer_invoice VALUES (0,2,"2023-11-27 12:30:08", 2),
 (0,1,"2023-11-25 12:38:00",1),
-(1,0,"2023-11-26 10:30:08",5)
+(1,0,"2023-11-26 10:30:08",5);
